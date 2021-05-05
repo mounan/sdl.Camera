947fb712e1cd748c9a1b8b9750ef2c3f3d407a36
mounan <1173359575zmn@gmail.com>
2021-05-06 02:01:03 +0900

    20210506_ZOU added camera action, be able to display high resolution photo, add onSaveInstanceState for  rotation

--- app/src/main/java/jp/ac/titech/itpro/sdl/camera/MainActivity.java
+++ app/src/main/java/jp/ac/titech/itpro/sdl/camera/MainActivity.java
@@ -5,19 +5,32 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private final static int REQ_PHOTO = 1234;
-    private Bitmap photoImage = null;
+    private static final String TAG = "ROTATE";
+    private Uri photoURI = null;
+    String currentPhotoPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,26 +39,56 @@ public class MainActivity extends AppCompatActivity {
 
         Button photoButton = findViewById(R.id.photo_button);
         photoButton.setOnClickListener(v -> {
-            Intent intent = new Intent();
-            // TODO: You should setup appropriate parameters for the intent
-
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
             PackageManager manager = getPackageManager();
             @SuppressLint("QueryPermissionsNeeded")
             List<ResolveInfo> activities = manager.queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY);
-            if (!activities.isEmpty()) {
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+            }
+            if (!activities.isEmpty() && photoFile != null) {
+                photoURI = FileProvider.getUriForFile(this,
+                        "jp.ac.titech.itpro.sdl.camera.fileprovider",
+                        photoFile);
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
                 startActivityForResult(intent, REQ_PHOTO);
             } else {
                 Toast.makeText(MainActivity.this, R.string.toast_no_activities, Toast.LENGTH_LONG).show();
             }
         });
+        Log.d(TAG, "onCreate: savedInstanceState out");
+        if (savedInstanceState != null) {
+            photoURI = savedInstanceState.getParcelable("photoURI");
+            ImageView photoView = findViewById(R.id.photo_view);
+            photoView.setImageURI(photoURI);
+        }
     }
 
     private void showPhoto() {
-        if (photoImage == null) {
+        if (photoURI  == null) {
             return;
         }
         ImageView photoView = findViewById(R.id.photo_view);
-        photoView.setImageBitmap(photoImage);
+        photoView.setImageURI(photoURI);
+    }
+
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
     }
 
     @Override
@@ -53,7 +96,7 @@ public class MainActivity extends AppCompatActivity {
         super.onActivityResult(reqCode, resCode, data);
         if (reqCode == REQ_PHOTO) {
             if (resCode == RESULT_OK) {
-                // TODO: You should implement the code that retrieve a bitmap image
+                showPhoto();
             }
         }
     }
@@ -62,5 +105,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
         showPhoto();
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putParcelable("photoURI", photoURI);
     }
 }
